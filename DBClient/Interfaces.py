943f7868bafac2fdd08d416f91039c6306732320
Strupervlg <53981215+Strupervlg@users.noreply.github.com>
2021-05-15 23:12:55 +0300

    Добавил интерфейс для класса Game

---
+++ DBClient/Interfaces.py
@@ -0,0 +1,17 @@
+from Entities import *
+
+class IGameRepository(Game):
+    def getAll(self):
+        pass
+
+    def getById(self, id: int):
+        pass
+
+    def add(self, game: Game):
+        pass
+
+    def update(self, game: Game):
+        pass
+
+    def delete(self, game: Game):
+        pass

95e9d57c488adb3555e4dd8abcff7246b50d5934
Strupervlg <53981215+Strupervlg@users.noreply.github.com>
2021-05-15 23:43:46 +0300

    Создал интерфейс для класса GameSeries

--- DBClient/Interfaces.py
+++ DBClient/Interfaces.py
@@ -15,3 +15,19 @@ class IGameRepository(Game):
 
     def delete(self, game: Game):
         pass
+
+class IGameSeriesRepository(GameSeries):
+    def getAll(self):
+        pass
+
+    def getById(self, id: int):
+        pass
+
+    def add(self, gameSeries: GameSeries):
+        pass
+
+    def update(self, gameSeries: GameSeries):
+        pass
+
+    def delete(self, gameSeries: GameSeries):
+        pass

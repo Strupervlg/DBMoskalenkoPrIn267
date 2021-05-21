e6c88fdb0c34ae324a65e93c4a4c8e1c45347815
Strupervlg <53981215+Strupervlg@users.noreply.github.com>
2021-05-21 15:36:29 +0300

    Добавил комментарии для класса IGameRepository

--- DBClient/Interfaces.py
+++ DBClient/Interfaces.py
@@ -1,22 +1,52 @@
 from Entities import *
 
 class IGameRepository(Game):
+    """
+    Интерфейс для репозитория игры
+    """
     def getAll(self):
+        """
+        Получение всех игры
+        :return: список игр
+        """
         pass
 
     def getById(self, id: int):
+        """
+        Получение игры по айди
+        :param id: айди игры
+        :return: игра
+        """
         pass
 
     def add(self, game: Game):
+        """
+        Добавление игры в БД
+        :param game: игра
+        :return: игра с айди из БД
+        """
         pass
 
     def update(self, game: Game):
+        """
+        Обновление игры в БД
+        :param game: обновленная игра
+        """
         pass
 
     def delete(self, game: Game):
+        """
+        Удаление игры из БД
+        :param game: игра для удаления
+        """
         pass
 
     def getAvgRating(self, game: Game):
+        """
+        Получение среднего рейтинга игры
+        :param game: игра для расчета рейтинга
+        :return: средний рейтинг игры
+        """
         pass
 
 class IGameSeriesRepository(GameSeries):
